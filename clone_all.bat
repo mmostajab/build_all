b1698557b832946b750b361f2b4b9461a47ea635
Morteza Mostajab <mmostajab@gmail.com>
2020-12-23 19:17:33 +0100

    + change vk_raytracing_tutorial_KHR to my forked repo

--- clone_all.bat
+++ clone_all.bat
@@ -2,46 +2,51 @@
 pushd ..
 
 :: do something cool, then log it
-CALL :folder build_all
-CALL :folder shared_sources
-CALL :folder shared_external
+CALL :nvfolder build_all
+CALL :nvfolder shared_sources
+CALL :nvfolder shared_external
 
-CALL :folder gl_cadscene_rendertechniques
-CALL :folder gl_commandlist_basic
-CALL :folder gl_dynamic_lod
-CALL :folder gl_multicast
-CALL :folder gl_occlusion_culling
-CALL :folder gl_path_rendering_CMYK
-CALL :folder gl_render_vk_direct_display
-CALL :folder gl_ssao
-CALL :folder gl_vk_bk3dthreaded
-CALL :folder gl_vk_chopper
-CALL :folder gl_vk_meshlet_cadscene
-CALL :folder gl_vk_raytrace_interop
-CALL :folder gl_vk_simple_interop
-CALL :folder gl_vk_supersampled
-CALL :folder gl_vk_threaded_cadscene
-CALL :folder nvml_enterprise_gpu_check
-CALL :folder vk_async_resources
-CALL :folder vk_denoise
-CALL :folder vk_device_generated_cmds
-CALL :folder vk_mini_path_tracer
-CALL :folder vk_offline
-CALL :folder vk_order_independent_transparency
-CALL :folder vk_raytrace
-CALL :folder vk_raytracing_tutorial
-CALL :folder vk_raytracing_tutorial_KHR
-CALL :folder vk_shaded_gltfscene
-CALL :folder vk_toon_shader
+CALL :nvfolder gl_cadscene_rendertechniques
+CALL :nvfolder gl_commandlist_basic
+CALL :nvfolder gl_dynamic_lod
+CALL :nvfolder gl_multicast
+CALL :nvfolder gl_occlusion_culling
+CALL :nvfolder gl_path_rendering_CMYK
+CALL :nvfolder gl_render_vk_direct_display
+CALL :nvfolder gl_ssao
+CALL :nvfolder gl_vk_bk3dthreaded
+CALL :nvfolder gl_vk_chopper
+CALL :nvfolder gl_vk_meshlet_cadscene
+CALL :nvfolder gl_vk_raytrace_interop
+CALL :nvfolder gl_vk_simple_interop
+CALL :nvfolder gl_vk_supersampled
+CALL :nvfolder gl_vk_threaded_cadscene
+CALL :nvfolder nvml_enterprise_gpu_check
+CALL :nvfolder vk_async_resources
+CALL :nvfolder vk_denoise
+CALL :nvfolder vk_device_generated_cmds
+CALL :nvfolder vk_mini_path_tracer
+CALL :nvfolder vk_offline
+CALL :nvfolder vk_order_independent_transparency
+CALL :nvfolder vk_raytrace
+CALL :nvfolder vk_raytracing_tutorial
+CALL :mmfolder vk_raytracing_tutorial_KHR
+CALL :nvfolder vk_shaded_gltfscene
+CALL :nvfolder vk_toon_shader
 
 :: force execution to quit at the end of the "main" logic
 EXIT /B %ERRORLEVEL%
 
 :: a function to write to a log file and write to stdout
-:folder
+:nvfolder
 IF EXIST %* GOTO NOWINDIR
 	echo "--------------------------------------------"
 	git clone https://github.com/nvpro-samples/%*.git --recursive
+	GOTO DONE
+:mmfolder
+IF EXIST %* GOTO NOWINDIR
+	echo "--------------------------------------------"
+	git clone https://github.com/mmostajab/%*.git --recursive
 	GOTO DONE
 :NOWINDIR
 	echo "--------------------------------------------"
